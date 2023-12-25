da51535792cf8ffed7d940830ed483b7e7e4d0f4
Najmul shaon <najmul.nh.shaon@gmail.com>
2023-12-25 17:56:40 +0600

    useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,17 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import Users from "./Users";
 
 function App() {
   const [count, setCount] = useState(0);
 
-  return <></>;
+  return (
+    <>
+      <h1>useEffect</h1>
+      <Users></Users>
+    </>
+  );
 }
 
 export default App;
